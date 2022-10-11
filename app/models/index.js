73b48ee6909d5a2e37a48a01e00b6236d94b9a05
gilangcsy <gilangchandra9@gmail.com>
2022-10-11 21:07:32 +0700

    mengubah .env.example

--- app/models/index.js
+++ app/models/index.js
@@ -4,14 +4,14 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    dialect: 'postgres',
     operatorAliases: false,
-    timezone: process.env.DB_TIMEZONE,
+    timezone: '+07:00',
     pool: {
-        max: parseInt(process.env.DB_POOL_MAX),
-        min: parseInt(process.env.DB_POOL_MIN),
-        acquire: parseInt(process.env.DB_POOL_ACQUIRE),
-        idle: parseInt(process.env.DB_POOL_IDLE)
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
     },
     debug:  false,
     dateStrings: 'date'
